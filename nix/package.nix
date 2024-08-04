7de3f276423fa50a25017021b473cbb5abd0194b
NotAShelf <raf@notashelf.dev>
2024-08-05 02:04:23 +0300

    nix: build with llvm.stdenv; use mold linker

--- nix/package.nix
+++ nix/package.nix
@@ -1,17 +1,21 @@
 {
   lib,
   rustPlatform,
+  stdenvAdapters,
+  llvm,
 }: let
   toml = (lib.importTOML ../Cargo.toml).package;
   pname = toml.name;
   inherit (toml) version;
 in
-  rustPlatform.buildRustPackage {
+  rustPlatform.buildRustPackage.override {stdenv = stdenvAdapters.useMoldLinker llvm.stdenv;} {
+    RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+
     inherit pname version;
 
     src = builtins.path {
       name = "${pname}-${version}";
-      path = ../.;
+      path = lib.sources.cleanSource ../.;
     };
 
     cargoLock.lockFile = ../Cargo.lock;
