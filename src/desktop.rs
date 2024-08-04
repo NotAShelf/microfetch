72d3130aa098a9e99dbca264a96e2372a1740204
NotAShelf <raf@notashelf.dev>
2024-08-04 22:04:23 +0300

    clean up get_desktop_info; increment minor version

--- src/desktop.rs
+++ src/desktop.rs
@@ -1,26 +1,19 @@
 use std::{env, io};
 
 pub fn get_desktop_info() -> Result<String, io::Error> {
-    let desktop_env = env::var("XDG_CURRENT_DESKTOP").unwrap_or_default();
-    let display_backend = env::var("XDG_SESSION_TYPE").unwrap_or_default();
+    let desktop_env = env::var("XDG_CURRENT_DESKTOP");
+    let display_backend = env::var("XDG_SESSION_TYPE");
 
     // Trim "none+" from the start of desktop_env if present
     // XXX: This is a workaround for NixOS modules that set XDG_CURRENT_DESKTOP to "none+foo"
     // instead of just "foo"
-    let desktop_env = desktop_env.trim_start_matches("none+");
-
     // Use "Unknown" if desktop_env or display_backend is empty
-    let desktop_env = if desktop_env.is_empty() {
-        "Unknown"
-    } else {
-        desktop_env
+    let desktop_env = match desktop_env {
+        Err(_) => String::from("Unknown"),
+        Ok(s) => s.trim_start_matches("none+").to_owned(),
     };
 
-    let display_backend = if display_backend.is_empty() {
-        "Unknown"
-    } else {
-        &display_backend
-    };
+    let display_backend = display_backend.unwrap_or_else(|_| String::from("Unknown"));
 
     Ok(format!("{desktop_env} ({display_backend})"))
 }
